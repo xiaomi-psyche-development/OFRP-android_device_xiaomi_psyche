193b1030770298cb7cafc1505f424bec52fd89db
Darth9 <reyprimerjefe@gmail.com>
2024-03-18 21:15:53 +0000

    Reformat and tidy up

--- Android.mk
+++ Android.mk
@@ -3,9 +3,13 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Copyright (C) 2021-2024 The OrangeFox Recovery Project
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
 
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter alioth,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
+#
