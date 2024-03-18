eb974d36b8e3bacaff8609e6c27e79eb72a0d608
Darth9 <reyprimerjefe@gmail.com>
2024-03-18 20:42:05 +0000

    Update the unified script

--- recovery/root/system/bin/unified-script.sh
+++ recovery/root/system/bin/unified-script.sh
@@ -1,6 +1,41 @@
 #!/system/bin/sh
+#
 # set some props, depending on device
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2021-2024 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
 
+setdevicespecs() {
+    resetprop "ro.product.name" "$1"
+    resetprop "ro.build.product" "$1"
+    resetprop "ro.vendor.product.device" "$1"
+    resetprop "ro.system.product.device" "$1"
+    resetprop "ro.system_ext.product.device" "$1"
+    resetprop "ro.odm.product.device" "$1"
+    resetprop "ro.product.device" "$1"
+    resetprop "ro.product.product.device" "$1"
+    resetprop "ro.product.bootimage.device" "$1"
+    resetprop "ro.product.odm.device" "$1"
+    resetprop "ro.product.system.device" "$1"
+    resetprop "ro.product.system_ext.device" "$1"
+    resetprop "ro.product.vendor.device" "$1"
+}
 
 load_INDIA()
 {
@@ -16,14 +51,7 @@ load_INDIA()
     resetprop "ro.product.system_ext.brand" "Xiaomi"
     resetprop "ro.product.product.brand" "Xiaomi"
     resetprop "ro.product.vendor.brand" "Xiaomi"
-    resetprop "ro.product.name" "aliothin"
-    resetprop "ro.build.product" "aliothin"
-    resetprop "ro.product.device" "aliothin"
-    resetprop "ro.vendor.product.device" "aliothin"
-    resetprop "ro.system.product.device" "aliothin"
-    resetprop "ro.system_ext.product.device" "aliothin"
-    resetprop "ro.odm.product.device" "aliothin"
-    resetprop "ro.product.product.device" "aliothin"
+    setdevicespecs "aliothin"
 }
 
 load_CN()
@@ -40,6 +68,7 @@ load_CN()
     resetprop "ro.product.system_ext.brand" "Xiaomi"
     resetprop "ro.product.product.brand" "Xiaomi"
     resetprop "ro.product.vendor.brand" "Xiaomi"
+    setdevicespecs "alioth"
 }
 
 load_global()
@@ -56,6 +85,7 @@ load_global()
     resetprop "ro.product.system_ext.brand" "POCO"
     resetprop "ro.product.product.brand" "POCO"
     resetprop "ro.product.vendor.brand" "POCO"
+    setdevicespecs "alioth"
 }
 
 variant=$(getprop ro.boot.hwc)
