e4237dab08446b36374faaba5c2f4c665b5823f9
Darth9 <reyprimerjefe@gmail.com>
2024-03-18 21:03:23 +0000

    Update settings and inherit fox-specific flags

--- twrp_alioth.mk
+++ twrp_alioth.mk
@@ -1,18 +1,30 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Copyright (C) 2021-2024 The OrangeFox Recovery Project
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := alioth
+
+DEVICE_PATH := device/xiaomi/$(PRODUCT_RELEASE_NAME)
 
 # Inherit from alioth device
-$(call inherit-product, device/xiaomi/alioth/device.mk)
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit any OrangeFox-specific settings
+$(call inherit-product-if-exists, $(DEVICE_PATH)/fox_mikona.mk)
+
+# Inherit some common twrp stuff
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Device identifier. This must come after all inclusions.
-PRODUCT_NAME := twrp_alioth
-PRODUCT_DEVICE := alioth
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
+PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
 PRODUCT_BRAND := POCO
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := POCO F3
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+#
